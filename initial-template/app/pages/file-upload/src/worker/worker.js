eb10d22c1d0da865f47ba279428a77d34413a339
Henrique Ducati <henrique.dcti@gmail.com>
2023-10-26 15:48:09 -0300

    feat(aula-01): added worker and view

---
+++ initial-template/app/pages/file-upload/src/worker/worker.js
@@ -0,0 +1,48 @@
+import CanvasRenderer from "./canvasRenderer";
+import MP4Demuxer from "./mp4Demuxer";
+import VideoProcessor from "./videoProcessor";
+
+const qvgaConstraints = {
+  width: 320,
+  height: 240,
+};
+
+const vgaConstraints = {
+  width: 640,
+  height: 480,
+};
+
+const hdContraints = {
+  width: 1280,
+  height: 720,
+};
+
+const encoderConfig = {
+  // WebM
+  ...qvgaConstraints,
+  bitrate: 10e6,
+  codec: "vp09.00.10.08",
+  pt: 4,
+  hardwareAcceleration: "prefer-software",
+};
+
+const mp4 = {
+  codec: "avc1.42002A",
+  pt: 1,
+  hardwareAcceleration: "prefer-hardware",
+  avc: { format: "annexb" },
+};
+
+const mp4Demuxer = new MP4Demuxer();
+const videoProcessor = new VideoProcessor({ mp4Demuxer });
+
+onmessage = async ({ data }) => {
+  const renderFrame = CanvasRenderer.getRenderer(data.canvas);
+  await videoProcessor.start({
+    file: data.file,
+    renderFrame,
+    encoderConfig,
+  });
+
+  self.postMessage({ status: "done" });
+};
